3ed6e2aa2a4ae2cc1fc98bc80a94cf11482eeb89
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-12-02 00:19:42 +0200

    Remove unused tags from table on remove/edit

--- src/repositories/tag_repository.py
+++ src/repositories/tag_repository.py
@@ -50,6 +50,14 @@ def add_tags_to_reference(reference_id: int, tag_names: list[str], *, commit: bo
 def delete_tags_from_reference(reference_id: int, *, commit: bool = True):
     sql = text("DELETE FROM reference_taggins WHERE reference_id = :reference_id")
     db.session.execute(sql, {"reference_id": reference_id})
+
+    # Remove any tags that are no longer in use
+    cleanup_sql = text(
+        "DELETE FROM tags t "
+        "WHERE NOT EXISTS (SELECT 1 FROM reference_taggins rt WHERE rt.tag_id = t.id)"
+    )
+    db.session.execute(cleanup_sql)
+
     if commit:
         db.session.commit()
 
